fb0f093c3f5836d271712bfdac707cdb62c55225
manel <im_mechouar@esi.dz>
2021-09-19 10:41:32 +0100

    new version

--- android/app/src/main/kotlin/com/example/day11/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/day11/MainActivity.kt
@@ -1,13 +1,8 @@
 package com.example.day11
 
-import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
 
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+class MainActivity: FlutterActivity()
+{
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -26,24 +26,29 @@ class _HomePageState extends State<HomePage> {
               height: 300,
               decoration: BoxDecoration(
                 image: DecorationImage(
-                  image: NetworkImage('https://mdfive.dz/ecole/wp-content/uploads/2020/12/background-scaled.jpg'),
-                  fit: BoxFit.cover
+                    image: NetworkImage(
+                        'https://mdfive.dz/ecole/wp-content/uploads/2020/12/background-scaled.jpg'),
+                    fit: BoxFit.cover
                 ),
               ),
               child: Container(
                 decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.bottomRight,
-                    colors: [
-                      Colors.black.withOpacity(.8),
-                      Colors.black.withOpacity(.2),
-                    ]
-                  )
+                    gradient: LinearGradient(
+                        begin: Alignment.bottomRight,
+                        colors: [
+                          Colors.black.withOpacity(.8),
+                          Colors.black.withOpacity(.2),
+                        ]
+                    )
                 ),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: <Widget>[
-                    FadeAnimation(1, Text("Révélez vos competences", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),)),
+                    FadeAnimation(1, Text(
+                      "Révélez vos competences", textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold),)),
                     SizedBox(height: 30,),
                     FadeAnimation(1.3, Container(
                       padding: EdgeInsets.symmetric(vertical: 3),
@@ -55,10 +60,11 @@ class _HomePageState extends State<HomePage> {
                       ),
                       child: TextField(
                         decoration: InputDecoration(
-                          border: InputBorder.none,
-                          prefixIcon: Icon(Icons.search, color: Colors.grey,),
-                          hintStyle: TextStyle(color: Colors.grey, fontSize: 15),
-                          hintText: "Recherchez votre formation"
+                            border: InputBorder.none,
+                            prefixIcon: Icon(Icons.search, color: Colors.grey,),
+                            hintStyle: TextStyle(
+                                color: Colors.grey, fontSize: 15),
+                            hintText: "Recherchez votre formation"
                         ),
                       ),
                     )),
@@ -73,33 +79,47 @@ class _HomePageState extends State<HomePage> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  FadeAnimation(1, Text("Catégories", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey[800], fontSize: 20),)),
+                  FadeAnimation(1, Text("Catégories", style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[800],
+                      fontSize: 20),)),
                   SizedBox(height: 20,),
                   FadeAnimation(1.4, Container(
                     height: 200,
                     child: ListView(
                       scrollDirection: Axis.horizontal,
                       children: <Widget>[
-                        makeItem(image: 'assets/images/canada.jpg', title: 'Canada'),
-                        makeItem(image: 'assets/images/Italy.jpg', title: 'Italy'),
-                        makeItem(image: 'assets/images/greece.jpg', title: 'Greece'),
-                        makeItem(image: 'assets/images/united-states.jpg', title: 'United States')
+                        makeItem(
+                            image: 'assets/images/canada.jpg', title: 'Canada'),
+                        makeItem(
+                            image: 'assets/images/Italy.jpg', title: 'Italy'),
+                        makeItem(
+                            image: 'assets/images/greece.jpg', title: 'Greece'),
+                        makeItem(image: 'assets/images/united-states.jpg',
+                            title: 'United States')
                       ],
                     ),
                   )),
 
                   SizedBox(height: 20,),
-                  FadeAnimation(1, Text("Best Hotels", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey[800], fontSize: 20),)),
-                  SizedBox(height: 20,),                
+                  FadeAnimation(1, Text("Best Hotels", style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[800],
+                      fontSize: 20),)),
+                  SizedBox(height: 20,),
                   FadeAnimation(1.4, Container(
                     height: 200,
                     child: ListView(
                       scrollDirection: Axis.horizontal,
                       children: <Widget>[
-                        makeItem(image: 'assets/images/united-states.jpg', title: 'Canada'),
-                        makeItem(image: 'assets/images/greece.jpg', title: 'Italy'),
-                        makeItem(image: 'assets/images/Italy.jpg', title: 'Greece'),
-                        makeItem(image: 'assets/images/canada.jpg', title: 'United States')
+                        makeItem(image: 'assets/images/united-states.jpg',
+                            title: 'Canada'),
+                        makeItem(
+                            image: 'assets/images/greece.jpg', title: 'Italy'),
+                        makeItem(
+                            image: 'assets/images/Italy.jpg', title: 'Greece'),
+                        makeItem(image: 'assets/images/canada.jpg',
+                            title: 'United States')
                       ],
                     ),
                   )),
@@ -112,30 +132,29 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-
   Widget makeItem({image, title}) {
     return AspectRatio(
       aspectRatio: 1 / 1,
       child: Container(
         margin: EdgeInsets.only(right: 15),
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          image: DecorationImage(
-            image: AssetImage(image),
-            fit: BoxFit.cover
-          )
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+                image: AssetImage(image),
+                fit: BoxFit.cover
+            )
         ),
         child: Container(
           padding: EdgeInsets.all(20),
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            gradient: LinearGradient(
-              begin: Alignment.bottomRight,
-              colors: [
-                Colors.black.withOpacity(.8),
-                Colors.black.withOpacity(.2),
-              ]
-            )
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                  begin: Alignment.bottomRight,
+                  colors: [
+                    Colors.black.withOpacity(.8),
+                    Colors.black.withOpacity(.2),
+                  ]
+              )
           ),
           child: Align(
             alignment: Alignment.bottomLeft,
@@ -144,5 +163,124 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
     );
+
+
+    /*class CountryListTile extends StatelessWidget {
+  final String label;
+  final String countryName;
+  final int noOfTours;
+  final double rating;
+  final String imgUrl;
+  CountryListTile(
+  {@required this.title,
+  @required this.date,
+  @required this.time,
+  @required this.rating,
+  @required this.imgUrl});
+
+  @override
+  Widget build(BuildContext context) {
+  return Container(
+  margin: EdgeInsets.only(right: 8),
+  child: Stack(
+  children: [
+  ClipRRect(
+  borderRadius: BorderRadius.circular(16),
+  child: Container(
+  child: CachedNetworkImage(
+  image: imgUrl,
+  height: 220,
+  width: 150,
+  fit: BoxFit.cover,
+  ),)
+  ),
+  Container(
+  height: 200,
+  width: 150,
+  child: Column(
+  children: [
+  Row(
+  children: [
+  Container(
+  margin: EdgeInsets.only(left: 8, top: 8),
+  padding:
+  EdgeInsets.symmetric(vertical: 6, horizontal: 8),
+  decoration: BoxDecoration(
+  borderRadius: BorderRadius.circular(8),
+  color: Colors.white38),
+  child: Text(
+  label ?? "New",
+  style: TextStyle(color: Colors.white),
+  ))
+  ],
+  ),
+  Spacer(),
+  Row(
+  children: [
+  Container(
+  margin: EdgeInsets.only(bottom: 10, left: 8, right: 8),
+  child: Column(
+  crossAxisAlignment: CrossAxisAlignment.start,
+  children: [
+  Container(
+  child: Text(
+  "Thailand",
+  style: TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.w600,
+  fontSize: 16),
+  ),
+  ),
+  SizedBox(
+  height: 3,
+  ),
+  Text(
+  "18 Tours",
+  style: TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.w600,
+  fontSize: 13),
+  )
+  ],
+  ),
+  ),
+  Spacer(),
+  Container(
+  margin: EdgeInsets.only(bottom: 10, right: 8),
+  padding:
+  EdgeInsets.symmetric(horizontal: 3, vertical: 7),
+  decoration: BoxDecoration(
+  borderRadius: BorderRadius.circular(3),
+  color: Colors.white38),
+  child: Column(
+  children: [
+  Text(
+  "4.5",
+  style: TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.w600,
+  fontSize: 13),
+  ),
+  SizedBox(
+  height: 2,
+  ),
+  Icon(
+  Icons.star,
+  color: Colors.white,
+  size: 20,
+  )
+  ],
+  ))
+  ],
+  )
+  ],
+  ),
+  )
+  ],
+  ),
+  );
+  }
+  }*/
+
   }
 }
